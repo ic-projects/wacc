93558aba4ccbac701902f934cbb180e3523a7396
Jordan Spooner <jordan.spooner16@imperial.ac.uk>
2017-11-02 17:54:12 +0000

    Significantly speed up test suite by saving ASTs locally

--- test_suite/lib/testsuite.rb
+++ test_suite/lib/testsuite.rb
@@ -71,7 +71,8 @@ class TestSuite
              }
     end
 
-    expected = `#{@refCompile} -t #{source}`
+    ast_file = File.dirname(source) + "/" +  File.basename(source, ".wacc") + ".ast"
+    expected = File.read(ast_file)
     run_results = Utils.run3("/usr/bin/timeout",
                        ["--kill-after=5", "3", @compile, "-t", source],
                        nil, 1024 * 1024 * 100)
