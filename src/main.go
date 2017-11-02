adc9b2044bec2531dac5777ee04e304ec9d80f94
Alexander Clarke <ac3716@ic.ac.uk>
2017-11-02 15:53:16 +0000

    Started use of options

--- src/main.go
+++ src/main.go
@@ -4,13 +4,27 @@ import (
     "fmt"
     "log"
     "os"
+    "flag"
 )
 
 func main() {
-    tree, err := ParseFile(os.Args[1])
+  printTree := flag.Bool("t", false, "Prints the AST created by the parser")
+  semanticCheck := flag.Bool("s", false, "Parse the file for syntax and symantic errors and generate an AST")
+  flag.Parse()
+  var tree interface{}
+  if (len(os.Args) > 1) {
+    var err error
+    tree, err = ParseFile(os.Args[len(os.Args) - 1])
     if err != nil {
         log.Fatal(err)
     }
+  } else {
+    fmt.Println("Error: No file provided")
+  }
+  if (*semanticCheck) {
 
+  }
+  if (*printTree) {
     fmt.Println(tree)
+  }
 }
