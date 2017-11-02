442ffb5d7b25c4cb9cfe95cf64051776bdebe70c
Jordan Spooner <jordan.spooner16@imperial.ac.uk>
2017-11-02 16:24:46 +0000

    Print current process in main

--- src/main.go
+++ src/main.go
@@ -1,30 +1,51 @@
-package main;
+package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "flag"
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
 )
 
-func main() {
-  printTree := flag.Bool("t", false, "Prints the AST created by the parser")
-  semanticCheck := flag.Bool("s", false, "Parse the file for syntax and symantic errors and generate an AST")
-  flag.Parse()
-  var tree interface{}
-  if (len(os.Args) > 1) {
-    var err error
-    tree, err = ParseFile(os.Args[len(os.Args) - 1])
-    if err != nil {
-        log.Fatal(err)
-    }
-  } else {
-    fmt.Println("Error: No file provided")
-  }
-  if (*semanticCheck) {
+func number(s string) string {
+	var buf bytes.Buffer
+	for i, line := range strings.Split(s, "\n") {
+		if line != "" {
+			buf.WriteString(fmt.Sprintf("%d\t%s\n", i, line))
+		}
+	}
+	return buf.String()
+}
 
-  }
-  if (*printTree) {
-    fmt.Println(tree)
-  }
+func main() {
+	printTree := flag.Bool("t", false, "Prints the AST created by the parser")
+	semanticCheck := flag.Bool("s", false, "Parse the file for syntax and symantic errors and generate an AST")
+	filepath := os.Args[len(os.Args)-1]
+	flag.Parse()
+	var tree interface{}
+	if len(os.Args) > 1 {
+		var err error
+		tree, err = ParseFile(filepath)
+		fmt.Println("-- Compiling...")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("Error: No file provided")
+	}
+	if *semanticCheck {
+
+	}
+	if *printTree {
+		fmt.Println("-- Printing AST...")
+		fmt.Print(strings.TrimSuffix(path.Base(filepath), ".wacc"))
+		fmt.Println(".ast contents are:")
+		fmt.Println("===========================================================")
+		fmt.Print(tree)
+		fmt.Println("===========================================================")
+	}
+	fmt.Println("-- Finished")
 }
