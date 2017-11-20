c8e3ab72c4a42bad3da7713fbb86d7ceae79df3c
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-11-20 17:10:15 +0000

    Add String() function for Register type

--- src/gowacc/ast/code_generator.go
+++ src/gowacc/ast/code_generator.go
@@ -92,7 +92,49 @@ const (
 	LR
 	PC
 	APSR
+	UNDEFINED
 )
+
+func (r Register) String() string {
+	switch r {
+	case R0:
+		return "r0"
+	case R1:
+		return "r1"
+	case R2:
+		return "r2"
+	case R3:
+		return "r3"
+	case R4:
+		return "r4"
+	case R5:
+		return "r5"
+	case R6:
+		return "r6"
+	case R7:
+		return "r7"
+	case R8:
+		return "r8"
+	case R9:
+		return "r9"
+	case R10:
+		return "r10"
+	case R11:
+		return "r11"
+	case R12:
+		return "r12"
+	case SP:
+		return "sp"
+	case LR:
+		return "lr"
+	case PC:
+		return "pc"
+	case APSR:
+		return "apsr"
+	default:
+		return "UNDEFINED"
+	}
+}
 
 // GenerateCode is a function that will generate and return the finished assembly
 // code for a given AST.
