a94611a3d8ad881571b6a179eeff9b32640f087e
Jordan Spooner <jordan.spooner16@imperial.ac.uk>
2017-11-21 15:59:27 +0000

    Implement code generation for if-else statement nodes

--- src/gowacc/ast/code_generator.go
+++ src/gowacc/ast/code_generator.go
@@ -151,6 +151,7 @@ func GenerateCode(tree ProgramNode, symbolTable *SymbolTable) *Assembly {
 // Walk. It stores
 type CodeGenerator struct {
 	asm             *Assembly
+	labelCount      int
 	currentFunction string
 	symbolTable     *SymbolTable
 	freeRegisters   *RegisterStack
@@ -161,6 +162,7 @@ type CodeGenerator struct {
 func NewCodeGenerator(symbolTable *SymbolTable) *CodeGenerator {
 	return &CodeGenerator{
 		asm:             NewAssembly(),
+		labelCount:      0,
 		symbolTable:     symbolTable,
 		freeRegisters:   NewRegisterStackWith([]Register{R4, R5, R6, R7, R8, R9, R10, R11, R12}),
 		returnRegisters: NewRegisterStack(),
@@ -192,14 +194,14 @@ func NewRegisterStackWith(registers []Register) *RegisterStack {
 }
 
 func (registerStack *RegisterStack) Pop() Register {
-	  if len(registerStack.stack) != 0 {
-      register := registerStack.stack[len(registerStack.stack) - 1];
-      registerStack.stack = registerStack.stack[:len(registerStack.stack) - 1]
-      return register;
-    } else {
-      fmt.Println("Internal compiler error")
-      return UNDEFINED
-    }
+	if len(registerStack.stack) != 0 {
+		register := registerStack.stack[len(registerStack.stack)-1]
+		registerStack.stack = registerStack.stack[:len(registerStack.stack)-1]
+		return register
+	} else {
+		fmt.Println("Internal compiler error")
+		return UNDEFINED
+	}
 }
 
 func (registerStack *RegisterStack) Push(register Register) {
@@ -237,6 +239,17 @@ func (v *CodeGenerator) addFunction(name string) {
 	v.currentFunction = name
 }
 
+// NoRecurse defines the nodes of the AST which should not be automatically
+// walked. This means we can visit the children in any way we choose.
+func (v *CodeGenerator) NoRecurse(programNode ProgramNode) bool {
+	switch node := programNode.(type) {
+	case IfNode:
+		return true
+	default:
+		return false
+	}
+}
+
 // Visit will apply the correct rule for the programNode given, to be used with
 // Walk.
 func (v *CodeGenerator) Visit(programNode ProgramNode) {
@@ -272,7 +285,23 @@ func (v *CodeGenerator) Visit(programNode ProgramNode) {
 	case PrintlnNode:
 
 	case IfNode:
-
+		thisNode := programNode.(IfNode)
+		// Cond
+		v.Visit(thisNode.expr)
+		r := v.returnRegisters.Pop()
+		v.freeRegisters.Push(r)
+		v.addCode(fmt.Sprintf("CMP %s, #0", r))
+		v.addCode(fmt.Sprintf("BEQ L%d", v.labelCount))
+		// If
+		v.Visit(thisNode.ifStats)
+		v.addCode(fmt.Sprintf("B L%d", v.labelCount))
+		// Else
+		v.addCode(fmt.Sprintf("L%d:", v.labelCount))
+		v.labelCount++
+		v.Visit(thisNode.elseStats)
+		// Fi
+		v.addCode(fmt.Sprintf("L%d:", v.labelCount))
+		v.labelCount++
 	case LoopNode:
 
 	case ScopeNode:
@@ -495,8 +524,6 @@ func (location *Location) String() string {
 	if location.register != UNDEFINED {
 		return location.register.String()
 	}
-
-	var buf bytes.Buffer
 
 	// Location is an address on the heap
 	if location.address != 0 {
