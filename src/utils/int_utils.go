a712e99aa13125c8e6f7f0d93ffbad11efdfd242
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-11-24 16:40:09 +0000

    Add comments for Max and Min functions in int_utils.go

--- src/utils/int_utils.go
+++ src/utils/int_utils.go
@@ -1,5 +1,6 @@
 package utils
 
+// Max returns the greater of two integer values
 func Max(x int, y int) int {
 	if x > y {
 		return x
@@ -7,6 +8,7 @@ func Max(x int, y int) int {
 	return y
 }
 
+// Min returns the smaller of two integer values
 func Min(x int, y int) int {
 	return -Max(-x, -y)
 }
