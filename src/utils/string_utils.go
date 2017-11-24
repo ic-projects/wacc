bf5fc3a799e643459833a714455de8e092eec465
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-11-24 17:26:43 +0000

    Improve clarity of comment for Indent

--- src/utils/string_utils.go
+++ src/utils/string_utils.go
@@ -2,12 +2,12 @@ package utils
 
 import (
 	"bytes"
-	"strings"
 	"fmt"
+	"strings"
 )
 
-// Indent is a function to Indent when printing the AST, given the string s, it indents
-// it with all previous indents plus the new Indent (sep)
+// Indent is a function to indent when printing the AST. Given the string s, it
+// indents the string with all previous indents plus the new Indent (sep).
 func Indent(s string, sep string) string {
 	var buf bytes.Buffer
 	for _, line := range strings.Split(s, "\n") {
