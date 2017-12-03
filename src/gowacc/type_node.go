68d72fcdf723dc0500cdf14bf86cab36cf64d737
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-12-03 15:35:27 +0000

    Add PointerTypeNode to type_node.go
    Also add String() method to the TypeNode interface

--- src/gowacc/type_node.go
+++ src/gowacc/type_node.go
@@ -8,6 +8,7 @@ import (
 // TypeNode is an empty interface for all types to implement.
 type TypeNode interface {
 	equals(TypeNode) bool
+	String() string
 }
 
 /**************** TYPE NODE HELPER FUNCTIONS ****************/
@@ -207,6 +208,27 @@ func (node StructTypeNode) String() string {
 func (node StructTypeNode) equals(t TypeNode) bool {
 	if arr, ok := toValue(t).(StructTypeNode); ok {
 		return arr.Ident == node.Ident
+	}
+	return false
+}
+
+type PointerTypeNode struct {
+	T TypeNode
+}
+
+func NewPointerTypeNode(t TypeNode) *PointerTypeNode {
+	return &PointerTypeNode{
+		T: t,
+	}
+}
+
+func (node PointerTypeNode) String() string {
+	return fmt.Sprintf("%s *", node.T.String())
+}
+
+func (node PointerTypeNode) equals(t TypeNode) bool {
+	if arr, ok := toValue(t).(PointerTypeNode); ok {
+		return arr.T == node.T
 	}
 	return false
 }
