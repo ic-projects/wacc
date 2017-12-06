81990564b62d877a56cbea7990a984a06cbec1cd
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-12-06 21:30:18 +0000

    Implement Visit case for PointerNewNode

--- src/gowacc/code_generator.go
+++ src/gowacc/code_generator.go
@@ -551,6 +551,49 @@ func (v *CodeGenerator) Visit(programNode ast.ProgramNode) {
 				n.MemoryOffset,
 			)
 		}
+	case *ast.PointerNewNode:
+		register := v.getFreeRegister()
+
+		if identDec, ok := v.symbolTable.SearchForIdent(node.Ident.Ident); ok {
+			ast.Walk(v, node.Ident)
+			valueReg := v.getReturnRegister()
+
+			if !identDec.Location.IsAddress() {
+				// Make space for the variable
+				v.addCode("LDR r0, =%d", ast.SizeOf(ast.ToValue(identDec.T)))
+				v.addCode("BL malloc")
+				v.addCode("MOV %s, r0", register)
+
+				// Move the variable value onto the heap space
+				v.addCode("%s %s, [%s]",
+					store(ast.SizeOf(ast.ToValue(identDec.T))),
+					valueReg,
+					register,
+				)
+
+				if identDec.Location.IsStackOffset() {
+					// Replace variable stored on stack with address to variable stored on
+					// heap.
+					v.addCode("STR %s, %s",
+						register,
+						v.LocationOf(identDec.Location))
+
+					// Change the Location type to be an address on the heap
+					identDec.Location.IsOnHeap = true
+				} else if identDec.Location.IsRegister() {
+					// Store the address on stack
+					v.addCode("PUSH {%s}", register)
+					v.currentStackPos += 4
+
+					// Change the Location type to be an address on the heap
+					identDec.Location.Register = utils.UNDEFINED
+					identDec.Location.IsOnHeap = true
+					identDec.Location.CurrentPos = v.currentStackPos
+				}
+			}
+			v.freeRegisters.Push(valueReg)
+		}
+		v.returnRegisters.Push(register)
 	case *ast.NewPairNode:
 		register := v.getFreeRegister()
 
